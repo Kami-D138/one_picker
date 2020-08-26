2fb60538602d5e18c5db5fc893e45ada1413121a
daiki kamikawabata <s21450030@gmail.com>
2020-08-26 16:30:37 +0900

    fix for the production env

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200824145550) do
+ActiveRecord::Schema.define(version: 20200727074855) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
